2fd534c3d9934e4907640fddbe8c740644ac1bf2
yuri.lima28 <guilherme.yurinhu@gmail.com>
2024-03-28 12:15:07 -0300

    Apresentando o Desafio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intelectah.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
